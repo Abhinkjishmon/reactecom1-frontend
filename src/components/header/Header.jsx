ecea09924ec30df50c696692a7c69c44e17f991e
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-15 23:19:52 +0530

    feb15 2319

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import css from './Header.module.css'
+import Logo from '../../assets/logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className={css.container}>
+        <div className={css.logo}>
+            <img src={Logo} alt="logo image" />
+        </div>
+
+        <div className={css.right}>
+            <div className={css.menu}>
+
+            </div>
+            <input type="text" className={css.search} />
+
+            <span>PNG icon of cart</span>
+        </div>
+    </div>
   )
 }
 
