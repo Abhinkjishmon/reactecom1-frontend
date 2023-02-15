ce10de63e0b983411a6e91f7d9b75dc603f2b1c1
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-15 23:54:33 +0530

    header design finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import css from './Header.module.css'
 import Logo from '../../assets/logo.png'
+import {CgShoppingCart} from 'react-icons/cg'
 
 function Header() {
-  return (
-    <div className={css.container}>
-        <div className={css.logo}>
-            <img src={Logo} alt="logo image" />
-        </div>
-
-        <div className={css.right}>
-            <div className={css.menu}>
-
+    return (
+        <div className={css.container}>
+            <div className={css.logo}>
+                <img src={Logo} alt="logo image" />
+                <span>aCart</span>
             </div>
-            <input type="text" className={css.search} />
 
-            <span>PNG icon of cart</span>
+            <div className={css.right}>
+                <div className={css.menu}>
+                    <ul className={css.menu}>
+                        <li>Collections</li>
+                        <li>Brands</li>
+                        <li>New</li>
+                        <li>Sales</li>
+                        <li>ENG</li>
+                    </ul>
+                </div>
+                <input placeholder='Search...' type="text" className={css.search} />
+
+                <CgShoppingCart className={css.cart} />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
