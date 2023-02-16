6f48bbd638e6910fda2d15811d36a52a22809b86
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-16 10:57:03 +0530

    feb16 firstcommit swiper started

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+function Slider() {
+  return (
+    <div className='s-container'>
+        <Swiper>
+            <SwiperSlide>1</SwiperSlide>
+            <SwiperSlide>2</SwiperSlide>
+            <SwiperSlide>3</SwiperSlide>
+        </Swiper>
+    </div>
+  )
+}
+
+export default Slider
