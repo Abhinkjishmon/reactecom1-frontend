6f49d5ae27db5178a3fc84299419a0026b276918
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-17 01:08:16 +0530

    featured product section ends

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import css from './Products.module.css';
 import Plane from '../../assets/plane.png';
 import { ProductsData } from '../../data/products';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 const Products = () => {
-    const [MenuProducts,setMenuProducts] = useState(ProductsData)
+    const [parent] = useAutoAnimate()
+    const [MenuProducts,setMenuProducts] = useState(ProductsData);
+
+    const filter = (type)=>{
+        setMenuProducts(ProductsData.filter((product)=>product.type === type))
+    }
   return (
     <div className={css.container}>
         <img src={Plane} alt="product-plane-image" />
@@ -12,17 +18,25 @@ const Products = () => {
 
         <div className={css.products}>
             <ul className={css.menu}>
-                <li>All</li>
-                <li>Skin Care</li>
-                <li>Conditioners</li>
-                <li>Foundations</li>
+                <li onClick={()=>setMenuProducts(ProductsData)}>All</li>
+                <li onClick={()=>filter("skin care")}>Skin Care</li>
+                <li onClick={()=>filter("conditioner")}>Conditioners</li>
+                <li onClick={()=>filter("foundation")}>Foundations</li>
             </ul>
 
-            <div className={css.list}>
+            <div className={css.list} ref={parent}>
                 {
                     MenuProducts.map((product, i)=>(
                         <div className={css.product}>
-                            products
+                            <div className="left-s">
+                                <div className="name">
+                                    <span>{product.name}</span>
+                                    <span>{product.detail}</span>
+                                </div>
+                                <span>{product.price}$</span>
+                                <div>Shop Now</div>
+                            </div>
+                            <img src={product.img} alt="product-image" className='img-p' />
                         </div>
                     ))
                 }
