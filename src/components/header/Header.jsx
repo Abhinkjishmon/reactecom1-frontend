54e99d000d4149d32938ff6698d2ab8500ca9aab
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-17 23:05:12 +0530

    header mobile size end

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Header.module.css'
 import Logo from '../../assets/logo.png'
-import {CgShoppingCart} from 'react-icons/cg'
+import { CgShoppingCart } from 'react-icons/cg'
+import { GoThreeBars } from 'react-icons/go'
 
 function Header() {
+    const [ShowMenu, setShowMenu] = useState(true)
+    const toggleMenu = () => {
+        setShowMenu((ShowMenu) => !ShowMenu)
+    }
     return (
         <div className={css.container}>
             <div className={css.logo}>
@@ -12,15 +17,18 @@ function Header() {
             </div>
 
             <div className={css.right}>
-                <div className={css.menu}>
-                    <ul className={css.menu}>
-                        <li>Collections</li>
-                        <li>Brands</li>
-                        <li>New</li>
-                        <li>Sales</li>
-                        <li>ENG</li>
-                    </ul>
+
+                <div className={css.bars} onClick={toggleMenu}>
+                    <GoThreeBars />
                 </div>
+
+                <ul className={css.menu} style={{ display: ShowMenu ? 'inherit' : 'none' }}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
                 <input placeholder='Search...' type="text" className={css.search} />
 
                 <CgShoppingCart className={css.cart} />
