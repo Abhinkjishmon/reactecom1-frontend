3fe2e7b803ca07891093d51f51b7422ab20238df
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-17 11:53:08 +0530

    testimonial top section end

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,15 +3,21 @@ import css from './Testimonials.module.css';
 import Hero from '../../assets/testimonialHero.png'
 
 const Testimonials = () => {
-  return (
-    <div className={css.testimonials}>
-        <div className={css.wrapper}>
-            <span>Top Rated</span>
-            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, commodi.</span>
-            <img src={Hero} alt="testimonial image" />
+    return (
+        <div className={css.testimonials}>
+            <div className={css.wrapper}>
+                <div className={css.container}>
+                    <span>Top Rated</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, commodi.</span>
+                </div>
+                <img src={Hero} alt="testimonial image" />
+                <div className={css.container}>
+                    <span>100k</span>
+                    <span>Happy Customers With Us</span>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Testimonials
